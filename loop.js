8c107466454c27cd5c97b3ce93b935d70fe06205
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-09-06 12:49:25 +0530

    javascript important topics

---
+++ loop.js
@@ -0,0 +1,11 @@
+let arr1 = 5
+
+// for(let i in arr1){
+//     console.log(arr1[i]*2);
+// }
+let i = 5;
+let ans = 1;
+for(let i=1; i<5 ; i++){
+    ans = ans*i;
+    console.log(ans);
+}
---
+++ script.js
@@ -0,0 +1,77 @@
+// // let a = 10;
+// // const b = 20;
+
+// // function d(){
+// //     console.log(a);
+// //     let c= 30;
+// //     console.log(c);
+// // }
+
+// // d();
+// // console.log(a);
+// // var c = 40;
+
+// let num = 11;
+
+// if(num%2 == 0){
+//     console.log("even no.");
+// }else{
+//     console.log("odd no");
+    
+// }
+
+// // let days = 366;
+
+// // if(days>365){
+// //     console.log("leap year");
+// // }else{
+// //    console.log("not leap year"); 
+// // }
+
+// let days = 5;
+
+// switch (days){
+//     case 1:
+//         console.log("monday");
+//     break;
+//     case 2:
+//         console.log("tuesday");
+//     break;
+//     case 3:
+//         console.log("wednesday");
+//     break;
+//     case 4:
+//         console.log("thrusday");
+//     break;
+//     case 5:
+//         console.log("friday");
+//     break;
+//     case 6:
+//         console.log("satarfday");
+//     break;
+//     case 7:
+//         console.log("Sunday");
+//     break;
+// }
+
+// let a = 2;
+// let b = 18;
+// let c = 15;
+
+// if(a>b){
+//     console.log("a is greater");
+// }else if(b>c){
+//     console.log("b is greater"); 
+// }else{
+//     console.log("c is greater");
+// }
+
+
+
+
+
+
+
+
+
+
