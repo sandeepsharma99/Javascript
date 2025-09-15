ffab569572c2b4d839551899ec2093ea5de849f1
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-09-15 12:27:32 +0530

    lexicalscope and scope chaining

--- closer.js
+++ closer.js
@@ -9,3 +9,8 @@ function outer(){
 
 let res = outer();
 console.log(res);
+
+/* 
+lexical scoping (acess of parent scope)
+acess of parent scope and if value not found then parent moves to its parent scope known as lexical scoping chanining
+*/
