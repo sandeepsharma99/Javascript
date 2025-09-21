b11409ac76b492c20c1bc233302a2739b8e6d10d
sandeepsharma99 <sandeepsharma9182@gmail.com>
2025-09-22 00:30:16 +0530

    Promis methods

--- Vanilla_JavaScript/asynchronous.js
+++ Vanilla_JavaScript/asynchronous.js
@@ -1,4 +1,17 @@
-const p1 = Promise.resolve("resolved1")
+const response = fetch ("https://api.github.com/users/im-lakshyaa")
+response.then((res)=>{
+    let data = res.jason();
+    return data;
+})
+.then((data)=>{
+    console.log(data)
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+/* Promise constructor method  */
+const p1 = Promise.resolve("resolved1") 
 
 const p2 = Promise.reject("reject");
 const p3 = Promise.resolve("resolve2");
@@ -12,7 +25,15 @@ const p3 = Promise.resolve("resolve2");
 //     console.log(err)
 // })
 
-Promise.allSettled([p1,p2,p3])
+/* Promise Methods */
+
+Promise.allSettled([p1,p2,p3])  /* Waits for all promises to settle (fulfilled or rejected) Method and returns an array of their outcomes. */
+
+Promise.all([p1,p2,p3]) /* Waits for all promises to resolve and returns their results as an array. If any promise is rejected, it immediately rejects. */
+
+Promise.any([p1,p2,p3])  /* Promise.any() Method resolves with the first fulfilled promise. If all are rejected, it rejects with an AggregateError */
+
+Promise.race([p1,p2,p3])  /* gives resolves or rejects as soon as the first promise settles.  */
 
 .then((rand)=>{
     console.log(rand)
--- Vanilla_JavaScript/folder/new.js
+++
@@ -1,8 +0,0 @@
-let a = 10;
-let b = 20;
-
-function  o(){
-    console.log(a)
-}
-o()
-
